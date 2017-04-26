9bc4514ded8ac312147e22675f47c0ad889f82fc
Spencer Smith <dontaskspencer@gmail.com>
2017-04-26 02:27:46 -0600

    Broke out multiple functions

--- random.js
+++ random.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var crypto = require('crypto');
-var request = require('request');
+var request = require('request').defaults({jar: true});
 
 var count = 0;
 var max = 100;
@@ -13,32 +13,41 @@ function helpRequest(url){
   }
   currentURL = url;
   count++;
-  request(currentURL, check);
+
+  var options = {
+    url: currentURL,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    }
+  }
+  request(options, check);
 }
 
 function check(err, res, body){
-  if(res){
-    var status = res.statusCode;
-    if(status != 404){
-      console.log(currentURL);
-    } else {
-      var pad = "   ";
-      if(count > 9)
-        pad = pad.substring(0, pad.length - 1);
-      if(count > 99)
-          pad = pad.substring(0, pad.length - 1);
-      console.log(count + pad + status + " " + currentURL);
-      helpRequest(randomURL());
-    }
+  if(res == null){
+    return console.log("No response?");
   }
-  else {
-    console.log("No response?");
+  var status = res.statusCode;
+  console.log(count + getPad() + status + " " + currentURL);
+  if(status == 404){
+    helpRequest(randomURL());
+  } else {
+    console.log(body);
   }
 }
 
 function randomURL(){
-  var base = "https://amazon.com/dp/B01";
+  var base = "https://www.github.com/dp/B01";
   var rand = crypto.randomBytes(4).toString('hex').toUpperCase();
   rand = rand.substring(0, rand.length - 1);
   return base + rand;
+}
+
+function getPad(){
+  var pad = "   ";
+  if(count > 9)
+    pad = pad.substring(0, pad.length - 1);
+  if(count > 99)
+    pad = pad.substring(0, pad.length - 1);
+  return pad;
 }
